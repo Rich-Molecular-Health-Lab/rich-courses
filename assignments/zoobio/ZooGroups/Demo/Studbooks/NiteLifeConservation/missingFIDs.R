dd3fcdd6f52b4993dfe32656cfb76e7cb8e3651e
aliciamrich <aliciamrich@gmail.com>
2025-03-10 14:29:38 -0500

    population lab

---
+++ assignments/zoobio/ZooGroups/Demo/Studbooks/NiteLifeConservation/missingFIDs.R
@@ -0,0 +1,62 @@
+missing_sire_ids <- setdiff(
+  unique(studbook$Sire[!is.na(studbook$Sire) & studbook$Sire != "0"]),
+  studbook$ID
+)
+
+missing_dam_ids <- setdiff(
+  unique(studbook$Dam[!is.na(studbook$Dam) & studbook$Dam != "0"]),
+  studbook$ID
+)
+
+new_sire_rows <- studbook %>%
+  filter(Sire %in% missing_sire_ids) %>%
+  group_by(Sire) %>%
+  slice_head(n = 1) %>%
+  ungroup() %>%
+  mutate(
+    ID            = Sire,
+    Sex           = "Male",
+    Last_Date     = Birth_Date,
+    Last_Location = Birth_Location
+  ) %>%
+  mutate(
+    Sire           = "0",
+    Dam            = "0",
+    Status         = "Deceased",
+    Age            = params$first_birth,
+    Birth_Date     = Last_Date - years(params$first_birth),
+    Birth_Location = NA,
+    yr_birth       = year(Last_Date) - params$first_birth,
+    yr_last        = year(Last_Date)
+  ) %>%
+  select(names(studbook))
+
+new_dam_rows <- studbook %>%
+  filter(Dam %in% missing_dam_ids) %>%
+  group_by(Dam) %>%
+  slice_head(n = 1) %>%
+  ungroup() %>%
+  mutate(
+    ID            = Dam,
+    Sex           = "Female",
+    Last_Date     = Birth_Date,
+    Last_Location = Birth_Location
+  ) %>%
+  mutate(
+    Sire           = "0",
+    Dam            = "0",
+    Status         = "Deceased",
+    Age            = params$first_birth,
+    Birth_Date     = Last_Date - years(params$first_birth),
+    Birth_Location = NA,
+    yr_birth       = year(Last_Date) - params$first_birth,
+    yr_last        = year(Last_Date)
+  ) %>%
+  select(names(studbook))
+
+
+studbook_filled <- bind_rows(studbook, new_sire_rows)
+
+studbook_filled <- studbook_filled %>% arrange(ID)
+
+studbook_filled
--- assignments/zoobio/ZooGroups/Demo/reactables.R
+++ assignments/zoobio/ZooGroups/Demo/reactables.R
@@ -1,7 +1,7 @@
 ID <- function(df) {
   colDef(
     header = tippy("ID", tooltip = "Studbook ID color-coded by sex (maroon = F, blue = M, green = Undetermined)"),
-    maxWidth = 80,
+    maxWidth = 70,
     cell = color_tiles(
       data       = df,
       color_ref  = "color",
@@ -57,11 +57,11 @@ Last_Date <- function(df) {
 Birth_Location <- function(df) {
   colDef(header = tippy("Born",
                         tooltip = "Location of birth (captive-born) or capture (wild-born)"),
-         maxWidth     = 200,
-         cell         = color_tiles(
+         maxWidth     = 70,
+         cell         = pill_buttons(
            data       = df,
-           color_by   = "birth_loc_num",
-           colors     = paletteer_d(colors$rand),
+           opacity    = 0.6,
+           color_ref  = "Birth_loc_color",
            box_shadow = TRUE
          )
   )
@@ -70,21 +70,57 @@ Birth_Location <- function(df) {
 Last_Location <- function(df) {
   colDef(header = tippy("Last Location",
                         tooltip = "Current institution (Alive) or institution at time of death (Deceased)"),
-         maxWidth = 150,
-         cell         = color_tiles(
+         maxWidth = 70,
+         cell         = pill_buttons(
            data       = df,
-           color_by   = "last_loc_num",
-           colors     = paletteer_d(colors$rand),
+           opacity    = 0.6,
+           color_ref  = "Last_loc_color",
            box_shadow = TRUE
          )
   )
 }
 
+Current_Location <- function(df) {
+  colDef(header = tippy("Current Location",
+                        tooltip = "Current institution"),
+         maxWidth = 70,
+         cell         = pill_buttons(
+           data       = df,
+           opacity    = 0.6,
+           color_ref  = "Last_loc_color",
+           box_shadow = TRUE
+         ),
+        style = JS("function(rowInfo, column, state) {
+                        const firstSorted = state.sorted[0]
+                        if (!firstSorted || firstSorted.id === 'Current_Location') {
+                          const prevRow = state.pageRows[rowInfo.viewIndex - 1]
+                          if (prevRow && rowInfo.values['Current_Location'] === prevRow['Current_Location']) {
+                            return { visibility: 'hidden' }
+                          }
+                        }
+                      }")
+  )
+}
+
+bubble_count <- function(df, name) {
+  colDef(name     = name,
+         maxWidth = 250,
+         align    = "center",
+         cell     = bubble_grid(
+           data   = df,
+           text_color    = "#ffffff",
+           bold_text     = TRUE,
+           brighten_text = TRUE,
+           colors  = paletteer_d(colors$seq),
+         )
+  )
+}
 
 Age <- function(df) {
   colDef(header = tippy("Age", tooltip = "Now (Alive) or at time of death (Deceased)"),
          maxWidth = 50,
-         cell = color_tiles(
+         align    = "center",
+         cell = pill_buttons(
            data       = df,
            colors     =  paletteer_d(colors$seq),
            opacity    = 0.4,
@@ -96,8 +132,8 @@ Age <- function(df) {
 
 Sire <- function(df) {
   colDef(header = tippy("Father", tooltip = "Studbook ID of Sire (0 if wildborn or unknown)"),
-         maxWidth = 80,
-         cell = color_tiles(
+         maxWidth = 70,
+         cell = pill_buttons(
            data       = df,
            colors     = colors$sire,
            opacity    = 0.6,
@@ -109,8 +145,8 @@ Sire <- function(df) {
 
 Dam <- function(df) {
   colDef(header = tippy("Mother", tooltip = "Studbook ID of Dam (0 if wildborn or unknown)"),
-         maxWidth = 80,
-         cell = color_tiles(
+         maxWidth = 70,
+         cell = pill_buttons(
            data       = df,
            colors     = colors$dam,
            opacity    = 0.6,
@@ -133,6 +169,26 @@ Rel_Contribution <- function(df) {
          ), maxWidth = 200)
 }
 
+inbred <- function(df) {
+  colDef(header = tippy("F",
+        tooltip = "Inbreeding coefficient (the probability that, at a random autosomal locus, the two alleles carried by the member are identical by descent relative to the pedigree.)"),
+         align    = "center",
+         maxWidth = 300,
+         cell     = data_bars(
+           data          = df,
+           text_position = "inside-end",
+           box_shadow    = TRUE,
+           fill_color    = paletteer_d(colors$div),
+           background    = "#ffffff00",
+           text_color    = "#ffffff",
+           bold_text     = TRUE,
+           brighten_text = TRUE,
+           number_fmt    = label_number(accuracy = 0.001)
+             )
+         )
+}
+
+
 studbook.cols <- function(df) {
 list(
   Status            = Status(),
@@ -148,8 +204,8 @@ list(
   color             = colDef(show = FALSE),
   yr_birth          = colDef(show = FALSE),
   yr_last           = colDef(show = FALSE),
-  birth_loc_num     = colDef(show = FALSE),
-  last_loc_num      = colDef(show = FALSE)
+  Birth_loc_color   = colDef(show = FALSE),
+  Last_loc_color    = colDef(show = FALSE)
   )
 }
 
@@ -166,17 +222,45 @@ founder.cols <- function(df) {
     color             = colDef(show = FALSE),
     yr_birth          = colDef(show = FALSE),
     yr_last           = colDef(show = FALSE),
-    birth_loc_num     = colDef(show = FALSE),
-    last_loc_num      = colDef(show = FALSE)
+    Birth_loc_color   = colDef(show = FALSE),
+    Last_loc_color    = colDef(show = FALSE)
   )
 }
+
+living.cols <- function(df) {
+  list(
+    ID                = ID(df),
+    Birth_Location    = Birth_Location(df),
+    Birth_Date        = Birth_Date(df),
+    Age               = Age(df),
+    Current_Location  = Current_Location(df),
+    Sire              = Sire(df),
+    Dam               = Dam(df),
+    inbred            = inbred(df),
+    N_Ancestors       = bubble_count(df, "Ancestors"),
+    N_Descendants     = bubble_count(df, "Descendants"),
+    N_Children        = bubble_count(df, "Offspring"),
+    N_Siblings        = bubble_count(df, "Siblings"),
+    Sex               = colDef(show = FALSE),
+    color             = colDef(show = FALSE),
+    yr_birth          = colDef(show = FALSE),
+    yr_last           = colDef(show = FALSE),
+    Birth_loc_color   = colDef(show = FALSE),
+    Last_loc_color    = colDef(show = FALSE)
+  )
+}
+
+living.groups <- list(
+  colGroup(name    = "Cummulative Counts",
+           columns = c("N_Ancestors", "N_Children", "N_Siblings"))
+)
 
 
 studbook.react <- function(df, cols, ...) {
   df %>%
     reactable(
-      theme               = flatly(),
-      height              = 700,
+      theme               = flatly(centered = TRUE),
+      height              = 900,
       sortable            = TRUE,
       resizable           = TRUE,
       filterable          = TRUE,
@@ -188,4 +272,57 @@ studbook.react <- function(df, cols, ...) {
       ...
     )
 }
+
+lambda.hover <- "Growth rate: Finite rate of population growth calculated as the geometric mean of changes in total population size over the past 5 years"
+
+pva.cols <- list(
+  N = colDef(header = tippy("N",
+    tooltip = "Demographic Population size: Number of currently living individuals in the population")
+  ),
+  Ne = colDef(header = tippy("Ne",
+     tooltip = "Effective population size (estimated from the rate of inbreeding per generation)"
+     ),
+     format = colFormat(digits = 0)
+  ),
+  Ne_over_N = colDef(header = tippy("Ne/N",
+      tooltip = "Effective population size relative to population size"
+      ),
+      format = colFormat(digits = 1)
+  ),
+  n_founder_reps = colDef(header = tippy("Founders",
+      tooltip = "Number of founders represented in the current population" )
+  ),
+  FGE = colDef(header = tippy("FGE",
+      tooltip = "Founder genome equivalents: expressed in units of the number of wild-caught, unrelated individuals (“founders”) that would produce the same gene diversity as the current population"
+      ),
+      format = colFormat(digits = 1)
+  ),
+  F_mean = colDef(header = tippy("F",
+      tooltip = "Mean inbreeding level" )
+  ),
+  mean_gen = colDef(header = tippy("Generations",
+     tooltip = "Mean generation for living individuals"
+     ),
+     format = colFormat(digits = 1)
+  ),
+  delta_F = colDef(header = tippy("ΔF",
+    tooltip = "ΔF is the per–generation increase in inbreeding" )
+  ),
+  GD = colDef(header = tippy("GD %",
+    tooltip = "Gene diversity: current percentage of founding gene diversity retained"
+    ),
+    format = colFormat(percent = TRUE)
+  ),
+  MK = colDef(header = tippy("MK",
+      tooltip = "Population mean kinship" )
+  ),
+  lambda2 = colDef(header = tippy("\u03BB",
+    tooltip =  lambda.hover)
+  )
+)
+
+
+
+
+
 
